ad6306ed874bd2b109df01a2c71da94f54f3efbb
Dmitry Demin <shepardiwe@gmail.com>
2026-04-02 22:22:29 +0700

    Add UserService.API

---
+++ UserService.API/Program.cs
@@ -0,0 +1,38 @@
+using Common.Database;
+using Common.Infrastructure;
+using Common.Infrastructure.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+);
+
+builder.Services.AddSingleton<PasswordHasher>();
+
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
+builder.Services.AddSingleton<JwtService>();
+
+var jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>()!;
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = jwtOptions.ToValidationParameters();
+    });
+
+builder.Services.AddMediatR(
+    cfg => cfg.RegisterServicesFromAssemblyContaining<UserService.Application.Commands.Login.LoginCommand>()
+);
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+await app.RunAsync();
