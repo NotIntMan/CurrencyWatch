30e805b712a92b447b9d37a993a3729d805cdea8
Dmitry Demin <shepardiwe@gmail.com>
2026-04-02 19:03:03 +0700

    Add JwtService

---
+++ Common.Infrastructure/Jwt/JwtOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Common.Infrastructure.Jwt;
+
+public class JwtOptions
+{
+    public required string SecretKey { get; init; }
+    public required string Issuer { get; init; }
+    public required string Audience { get; init; }
+    public int ExpirationMinutes { get; init; } = 15;
+
+    public TokenValidationParameters ToValidationParameters() => new()
+    {
+        ValidateIssuer = true,
+        ValidIssuer = Issuer,
+        ValidateAudience = true,
+        ValidAudience = Audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey)),
+        ValidateLifetime = true,
+    };
+}
---
+++ Common.Infrastructure/Jwt/JwtService.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Common.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Common.Infrastructure.Jwt;
+
+public class JwtService
+{
+    private readonly JwtOptions _options;
+
+    public JwtService(IOptions<JwtOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Name, user.Name),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            ]),
+            Expires = DateTime.UtcNow.AddMinutes(_options.ExpirationMinutes),
+            Issuer = _options.Issuer,
+            Audience = _options.Audience,
+            SigningCredentials = credentials,
+        };
+
+        var handler = new JsonWebTokenHandler();
+        return handler.CreateToken(descriptor);
+    }
+}
