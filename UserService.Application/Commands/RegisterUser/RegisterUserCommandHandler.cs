6940442d632774fe849ebaf6fd4a69ebcc77b1f7
Dmitry Demin <shepardiwe@gmail.com>
2026-04-02 19:04:29 +0700

    Add RegisterUserCommand

---
+++ UserService.Application/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Common.Database;
+using Common.Domain.Entities;
+using Common.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserService.Application.DTOs;
+
+namespace UserService.Application.Commands.RegisterUser;
+
+public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, UserDto>
+{
+    private readonly AppDbContext _db;
+    private readonly PasswordHasher _passwordHasher;
+
+    public RegisterUserCommandHandler(AppDbContext db, PasswordHasher passwordHasher)
+    {
+        _db = db;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<UserDto> Handle(RegisterUserCommand request, CancellationToken ct)
+    {
+        var exists = await _db.Users.AnyAsync(u => u.Name == request.Name, ct);
+        if (exists)
+        {
+            throw new InvalidOperationException($"User '{request.Name}' already exists.");
+        }
+
+        var user = new User
+        {
+            Name = request.Name,
+            PasswordHash = _passwordHasher.Hash(request.Password),
+        };
+
+        _db.Users.Add(user);
+        await _db.SaveChangesAsync(ct);
+
+        return new UserDto(user.Id, user.Name);
+    }
+}
